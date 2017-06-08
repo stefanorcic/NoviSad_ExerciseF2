4946fd73567f00762e392669d550fe9585b03e69
somename <someemail>
2017-06-08 20:01:54 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -25,7 +25,8 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		String result = "(0,1,E)";
 		
-		return null;
+		return result;
 	}
 }
