b6b5afd5736d50076f79fbf3b030ee61d1a6de72
somename <someemail>
2017-06-08 20:08:05 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,14 +8,21 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		PlanetExplorer pl = new PlanetExplorer(3,3, "");
 		String rower = pl.executeCommand("f");
-		String expected = "(0,1,E)";
+		String expected = "(0,1,N)";
 		assertEquals(expected, rower);
 	}
 	@Test
 	public void test_executeCommandBack() {
 		PlanetExplorer pl = new PlanetExplorer(3,3, "");
 		String rower = pl.executeCommand("b");
-		String expected = "(0,2,E)";
+		String expected = "(0,2,N)";
+		assertEquals(expected, rower);
+	}
+	@Test
+	public void test_executeCommandRF() {
+		PlanetExplorer pl = new PlanetExplorer(3,3, "");
+		String rower = pl.executeCommand("rf");
+		String expected = "(0,0,E)";
 		assertEquals(expected, rower);
 	}
 }
